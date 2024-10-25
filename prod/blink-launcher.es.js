730fd117e7bf134d783d24dcf8f8779691818c1d
matt.vidallon <matt.vidallon@cubeia.com>
2024-10-25 15:47:21 +0800

    🚀 Deploy web component v0.1.0 (web-component-prod) - 2024-10-25T07-47-21-078Z

--- prod/blink-launcher.es.js
+++ prod/blink-launcher.es.js
@@ -1,4 +1,4 @@
-/* Built at: 2024-10-25T07:46:46.650Z */
+/* Built at: 2024-10-25T07:47:14.817Z */
 var Yw = Object.defineProperty;
 var Qw = (t, e, n) => e in t ? Yw(t, e, { enumerable: !0, configurable: !0, writable: !0, value: n }) : t[e] = n;
 var U = (t, e, n) => Qw(t, typeof e != "symbol" ? e + "" : e, n);
